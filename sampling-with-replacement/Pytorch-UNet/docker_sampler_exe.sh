4309b005905335990a8b5305f75251bbb7bc673e
yl-to <ubuntu@ip-172-31-14-133.ec2.internal>
2021-06-08 22:20:59 +0000

    Unet code initial

---
+++ sampling-with-replacement/Pytorch-UNet/docker_sampler_exe.sh
@@ -0,0 +1,1 @@
+docker exec unet_test /bin/bash -c "python /mnt/train.py " | tee test_run.log
---
+++ sampling-with-replacement/Pytorch-UNet/docker_start.sh
@@ -0,0 +1,10 @@
+CONTAINER_IMAGE=763104351884.dkr.ecr.us-east-1.amazonaws.com/pytorch-training:1.8.1-gpu-py36-cu111-ubuntu18.04
+CONTAINER_NAME=unet_test
+docker run --rm -it -d --gpus all \
+                    --name $CONTAINER_NAME \
+                    --net=host --uts=host --ipc=host \
+                    --ulimit stack=67108864 --ulimit memlock=-1 \
+                    --security-opt seccomp=unconfined \
+		    -v $(pwd):/mnt \
+                    -v /home/ubuntu/data:/data \
+                    $CONTAINER_IMAGE
