cd1bf298fb525a368574218a97b85a2778c519af
Mansi Mane <mansmane@amazon.com>
2021-10-04 17:41:08 -0700

    Added conda setup script

--- BERT/scripts/setup_conda_env.sh
+++ BERT/scripts/setup_conda_env.sh
@@ -5,6 +5,6 @@
  cd $WORK_DIR
  git clone https://github.com/NVIDIA/apex
  cd apex
- pip install -v --disable-pip-version-check --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./ &> log_pytorch_latest_p37
+ pip install -v --disable-pip-version-check --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./ &> /fsx/logs/${WORK_DIR}_log_pytorch_latest_p37
  pip install tqdm tensorboard yacs
  
