d805454035cabf65d373588c35be8ce9476d4d0a
Mansi Mane <mansmane@amazon.com>
2021-10-13 23:01:19 -0700

    Updated pretraining script for Adam 4 node

--- BERT/scripts/run_pretraining_base_adamw_2_node_no_gns_no_autoscalar.sh
+++ BERT/scripts/run_pretraining_base_adamw_2_node_no_gns_no_autoscalar.sh
@@ -1,17 +1,4 @@
-#!/bin/bash
-
-# Copyright (c) 2019 NVIDIA CORPORATION. All rights reserved.
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Runs phase 1 pretraining on 2 node and phase 2 pretraining on 4 node
 
 # echo "Container nvidia build = " $NVIDIA_BUILD_ID
 # conda env 
@@ -29,7 +16,6 @@ num_gpus=${4:-8}
 warmup_proportion=${5:-"0.2843"}
 train_steps=${6:-7038}
 save_checkpoint_steps=${7:-8000}
-save_checkpoint_steps=${7:-2000}
 
 resume_training=${8:-"false"}
 create_logfile=${9:-"true"}
