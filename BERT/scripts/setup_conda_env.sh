f2ebcb3fda2add6affae58c63a86f28d034cfb19
Mansi Mane <mansmane@amazon.com>
2021-10-21 11:49:22 -0700

    Updated conda setup scripts

--- BERT/scripts/setup_conda_env.sh
+++ BERT/scripts/setup_conda_env.sh
@@ -5,6 +5,11 @@
  cd $WORK_DIR
  git clone https://github.com/NVIDIA/apex
  cd apex
- pip install -v --disable-pip-version-check --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./ &> /fsx/logs/${WORK_DIR}_log_pytorch_latest_p37
+ pip install -v --disable-pip-version-check --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" ./ &> /fsx/logs/log_pytorch_latest_p37
  pip install tqdm tensorboard yacs
- 
+
+# Copy conda env to fsx
+ #  wget https://repo.anaconda.com/miniconda/Miniconda3-py37_4.10.3-Linux-x86_64.sh
+# chmod +x Miniconda3-py37_4.10.3-Linux-x86_64.sh
+#  ./Miniconda3-py37_4.10.3-Linux-x86_64.sh
+#  cp -r /home/ubuntu/anaconda3/envs/pytorch_latest_p37 /fsx/conda/envs/
