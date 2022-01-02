864b7684417fa44e773e30a4b4f9bc5f8652d38a
Mansi Mane <mansmane@amazon.com>
2022-01-02 15:08:20 -0800

    Added few options

--- cifar/cifar_adascale.py
+++ cifar/cifar_adascale.py
@@ -16,6 +16,9 @@ from fairscale.optim import AdaScale
 import time
 import statistics
 from torch.distributed.algorithms.ddp_comm_hooks.default_hooks import fp16_compress_hook
+import torch.distributed as dist
+from torch.utils.tensorboard import SummaryWriter
+
 
 def set_random_seeds(random_seed=0):
 
@@ -24,6 +27,20 @@ def set_random_seeds(random_seed=0):
     torch.backends.cudnn.benchmark = False
     np.random.seed(random_seed)
     random.seed(random_seed)
+
+def get_rank():
+    if not dist.is_available():
+        return 0
+    if not dist.is_initialized():
+        return 0
+    return dist.get_rank()
+
+def get_world_size():
+    if not dist.is_available():
+        return 1
+    if not dist.is_initialized():
+        return 1
+    return dist.get_world_size()
 
 def evaluate(model, device, test_loader):
 
@@ -51,7 +68,9 @@ def main():
     random_seed_default = 0
     model_dir_default = "saved_models"
     model_filename_default = "resnet_distributed.pth"
-
+    adascale_scale_default = 1
+    weight_decaye_default = 1e-5
+    momentum_default = 0.9
     # Each process runs on 1 GPU device specified by the local_rank argument.
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("--local_rank", type=int,
@@ -60,11 +79,21 @@ def main():
     parser.add_argument("--batch_size", type=int, help="Training batch size for one process.",
                         default=batch_size_default)
     parser.add_argument("--learning_rate", type=float, help="Learning rate.", default=learning_rate_default)
+    parser.add_argument("--weight_decay", type=float, help="Weight decay.", default=weight_decaye_default)
+    parser.add_argument("--momentum", type=float, help="Momentum.", default=momentum_default)
     parser.add_argument("--random_seed", type=int, help="Random seed.", default=random_seed_default)
     parser.add_argument("--model_dir", type=str, help="Directory for saving models.", default=model_dir_default)
     parser.add_argument("--model_filename", type=str, help="Model filename.", default=model_filename_default)
     parser.add_argument("--resume", action="store_true", help="Resume training from saved checkpoint.")
     parser.add_argument("--use_adascale", action="store_true", help="Use adascale optimizer for training.")
+    parser.add_argument("--adascale_scale", type=int, help="Scale factor for adascale.", default=adascale_scale_default)
+
+    parser.add_argument("--use_fp16_compress", action="store_true", help="Use fp16 compression for training.")
+    parser.add_argument('--log_dir',
+                        default='./logs',
+                        type=str,
+                        help='log directory path.')
+
     argv = parser.parse_args()
 
     local_rank = argv.local_rank
@@ -76,13 +105,26 @@ def main():
     model_filename = argv.model_filename
     resume = argv.resume
     use_adascale = argv.use_adascale
-
+    use_fp16_compress = argv.use_fp16_compress
+    tensorboard_path = f'{argv.log_dir}/worker-{torch.distributed.get_rank()}'
+    weight_decay = argv.weight_decay
+    momentum = argv.momentum
     # Create directories outside the PyTorch program
     # Do not create directory here because it is not multiprocess safe
     '''
     if not os.path.exists(model_dir):
         os.makedirs(model_dir)
     '''
+    print(" Adascale hyperparameters")
+    print(" Base batch size: ", batch_size)
+    print(" Scale factor batch size: ", get_world_size())
+    print(" Batch size after scaling: ", batch_size*get_world_size())
+    print(" Number of steps : ")
+
+    if get_rank() == 0:
+        # tensorboard summary writer (by default created for all workers)
+        writer = SummaryWriter(tensorboard_path)
+
 
     model_filepath = os.path.join(model_dir, model_filename)
 
@@ -99,8 +141,8 @@ def main():
     device = torch.device("cuda:{}".format(local_rank))
     model = model.to(device)
     ddp_model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[local_rank], output_device=local_rank)
-    # Add fp14 compreesion
-    model.register_comm_hook(state=None, hook=fp16_compress_hook)
+    if use_fp16_compress:
+        ddp_model.register_comm_hook(state=None, hook=fp16_compress_hook)
 
     # We only save the model who uses device "cuda:0"
     # To resume, the device for the saved model would also be "cuda:0"
@@ -131,13 +173,15 @@ def main():
     criterion = nn.CrossEntropyLoss()
     if use_adascale:
         print(" INFO: Using Adascale ")
-        optimizer = AdaScale(optim.SGD(ddp_model.parameters(), lr=learning_rate, momentum=0.9, weight_decay=1e-5))
+        optimizer = AdaScale(optim.SGD(ddp_model.parameters(), lr=learning_rate, momentum=momentum, weight_decay=weight_decay))
     else:
         print(" INFO: Not using Adascale")
-        optimizer = optim.SGD(ddp_model.parameters(), lr=learning_rate, momentum=0.9, weight_decay=1e-5)
+        optimizer = optim.SGD(ddp_model.parameters(), lr=learning_rate, momentum=momentum, weight_decay=weight_decay)
     step_times = []
     # # Loop over the dataset multiple times
     step = 0
+    done = False
+    epoch = 0
     for epoch in range(num_epochs):
 
         print("Local Rank: {}, Epoch: {}, Training ...".format(local_rank, epoch))
@@ -146,6 +190,9 @@ def main():
         if epoch % 10 == 0:
             if local_rank == 0:
                 accuracy = evaluate(model=ddp_model, device=device, test_loader=test_loader)
+                if get_rank() == 0:
+                    writer.add_scalar(f'Val/Acc', accuracy, epoch)
+                    writer.flush()
                 torch.save(ddp_model.state_dict(), model_filepath)
                 print("-" * 75)
                 print("Epoch: {}, Accuracy: {}".format(epoch, accuracy))
@@ -156,28 +203,20 @@ def main():
         for data in train_loader:
             inputs, labels = data[0].to(device), data[1].to(device)
             optimizer.zero_grad()
-            # start = time.time()
-            # torch.cuda.nvtx.range_push('FORWARD-PASS')
+
             outputs = ddp_model(inputs)
             loss = criterion(outputs, labels)
-            # torch.cuda.nvtx.range_pop()  # FORWARD-PASS
 
-            # torch.cuda.nvtx.range_push('BACKWARD-PASS')
             loss.backward()
-            # torch.cuda.nvtx.range_pop()  # BACKWARD-PASS
-
-            # torch.cuda.nvtx.range_push('OPTIMIZER-STEP')
+            if get_rank() == 0:
+                writer.add_scalar(f'Train/Loss', accuracy, step)
+                writer.flush()
             optimizer.step()
-            # torch.cuda.nvtx.range_pop()  # OPTIMIZER-STEP
             step += 1
 
-    print(" INFO: Total steps: ", step)
 
-    #         end = time.time()
-    #         step_times.append((end-start)*1000)
-    # print(step_times)
-    # print("INFO: Std dev", statistics.stdev(step_times))
-    # print("INFO: Mean (msec)", statistics.mean(step_times))
+            end = time.time()
+    print(" INFO: Total steps: ", step)
 
 
 
